24c011273539cfd1fa9d790ecbb47a9265a65967
Amine Formateur <amine.devinfo@gmail.com>
2023-11-10 01:48:38 +0100

    first commit

--- App.tsx
+++ App.tsx
@@ -1,29 +1,30 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { createTheme, ThemeProvider } from "@rneui/themed";
-import Component from "./components/MyComponent";
+// import { SafeAreaProvider } from "react-native-safe-area-context";
+// import { createTheme, ThemeProvider } from "@rneui/themed";
+// import Component from "./components/MyComponent";
 
-const theme = createTheme({
-  lightColors: {},
-  darkColors: {},
-});
+// const theme = createTheme({
+//   lightColors: {},
+//   darkColors: {},
+// });
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <ThemeProvider theme={theme}>
-        <Component />
-      </ThemeProvider>
-    </SafeAreaProvider>
+    // <SafeAreaProvider>
+    //   <ThemeProvider theme={theme}>
+    //     <Component />
+    //   </ThemeProvider>
+    // </SafeAreaProvider>
+    <Text>Start Using RNE </Text>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
