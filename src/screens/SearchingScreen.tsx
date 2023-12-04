f7f11c29e3a353dfa4c6ad1cb4cae82a1b2a2cf8
Abdelhadi Habchi <ahabchi@eutelsat.com>
2023-12-04 12:10:56 +0100

    Create SearchingScreen by Amine

--- src/screens/SearchingScreen.tsx
+++ src/screens/SearchingScreen.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { Text, View } from "react-native";
-import { Button } from "@rneui/themed";
+import React, { useEffect } from "react";
+import { ActivityIndicator, Text, View, Alert } from "react-native";
+
 import { NavigationProp } from "@react-navigation/native";
 
 export default function SearchingScreen({
@@ -8,10 +8,37 @@ export default function SearchingScreen({
 }: {
   navigation: NavigationProp<any, any>;
 }) {
+  useEffect(() => {
+    setTimeout(() => {
+      Alert.alert("Congratulations !", " You have a new match !", [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel",
+        },
+        { text: "OK", onPress: () => navigation.navigate("ChatsScreen") },
+      ]);
+    }, 5000);
+  }, []);
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>SearchingScreen</Text>
-      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
+      <ActivityIndicator
+        color={"#FF007F"}
+        style={{ transform: [{ scaleX: 5 }, { scaleY: 5 }] }}
+        size={"large"}
+      />
+      <Text
+        style={{
+          fontSize: 20,
+          fontWeight: "600",
+          top: 200,
+          textAlign: "center",
+        }}
+      >
+        Searching for a stringer ...
+      </Text>
+
+      {/* <Button title="Go to Home" onPress={() => navigation.navigate("Home")} /> */}
     </View>
   );
 }
